b6c3cd6ca70b4446a2087da2b952c44c1378aad8
A_Duck <therealaduck@gmail.com>
2026-02-08 17:32:32 -0600

    setting up update and autorun

--- run.sh
+++ run.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
+set -e
 
+BOT_DIR="/home/duck/Yuuri-Bot"
+venv="$BOT_DIR/venv"
+
+cd "$BOT_DIR"
+echo  "Setting up virtual environment..."
+source "$VENV/bin/activate"
 while true
 do
-  echo "Checking for updates..."
+  echo "Updating Yuuri Bot..."
   git pull
-  pip install -r requirements.txt
+  pip install -r requirements.txt --upgrade --quiet
 
   echo "Bot is starting..."
   python3 main.py
