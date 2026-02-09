53610f061520ddcde98c823f173f149f1df7f592
A_Duck <therealaduck@gmail.com>
2026-02-08 19:53:50 -0600

    logging

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 import os
+import logging
 import sys
 import discord
 from discord import app_commands
 from discord.ext import commands
 
+#set logging
+logger = logging.getLogger("YuuriBot")
+logger.setLevel(logging.DEBUG)
+file_handler = logging.FileHandler("yuuribot.log")
+file_handler.setLevel(logging.DEBUG)
+formatter = logging.Formatter('[%(asctime)s] [%(levelname)s] %(message)s')
+file_handler.setFormatter(formatter)
+logger.addHandler(file_handler)
 TOKEN = os.getenv("YUURI_TOKEN")
 intents = discord.Intents.default()
 intents.members = True
@@ -12,25 +21,26 @@ bot = commands.Bot(command_prefix="!", intents=intents)
 
 TEST_GUILD_ID = 939897099024203778
 test_guild = discord.Object(id=TEST_GUILD_ID)
-
 if TOKEN is None:
     raise RuntimeError("RUNTIME ERROR CODE1: Discord Token not found in the environment variable!")
 
 @bot.event
 async def on_ready():
-    print(f"Logged in as {bot.user}")
+    logger.info(f"Logged in as {bot.user}")
 
     guild = discord.Object(id=TEST_GUILD_ID)
     bot.tree.copy_global_to(guild=guild)
     await bot.tree.sync(guild=guild)
 
-    print("Slash commands synced")
+    logger.info("Slash commands synced")
 
 @bot.tree.command(name="force_restart", description='For if Yuuri misbehaves.', guild=test_guild)
 async def force_restart(interaction: discord.Interaction):
+    logger.debug("caught force restart command.")
     if not await bot.is_owner(interaction.user):
         await interaction.response.send_message("Hey! Watch it! You cant tell me what to do!", ephemeral=True)
         return
+        logger.debug("is owner")
         await interaction.response.send_message("Oh alright Ill do as you say.", ephemeral=True)
         await bot.close()
         sys.exit(0)
