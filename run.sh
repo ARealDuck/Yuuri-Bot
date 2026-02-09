1a84c05e093b15b475ef68e3a4518ddf3cf5dfee
A_Duck <therealaduck@gmail.com>
2026-02-08 18:35:33 -0600

    setting up update and autorun

--- run.sh
+++ run.sh
@@ -1,17 +1,46 @@
 #!/bin/bash
-set -e
+set -e #exit if script fails for any reason
 
+#set variables
 BOT_DIR="/home/duck/Yuuri-Bot"
-venv="$BOT_DIR/venv"
+VENV="$BOT_DIR/venv"
+LOG_FILE="$BOT_DIR/bot.log"
 
+#redirect stdout and stderr to log file
+exec >> "$LOG_FILE" 2>&1
+
+#start script
 cd "$BOT_DIR"
+
+#create Venv environment if it does not exist.
+if [ ! -d "$VENV"]; then
+  echo "VENV not found creating new venv environment."
+  python3 -m venv "$VENV"
+fi
+
+#activate Venv environment
 echo  "Setting up virtual environment..."
 source "$VENV/bin/activate"
+
+#run auto update loop for the bot.
 while true
 do
   echo "Updating Yuuri Bot..."
   git pull
+  REQ_HASH_FILE="$VENV/.req_hash"
+  CURRENT_HASH=$(sha256sum requirements.txt | awk '{print $1}')
+  PREV_HASH=''
+  if [ -f "$REQ_HASH_FILE" ]; then
+    PREV_HASH=$(cat "$REQ_HASH_FILE")
+  fi
+
+  if [ "$CURRENT_HASH" != "$PREV_HASH" ]; then
+    echo "Installing / updating dependencies..."
   pip install -r requirements.txt --upgrade --quiet
+    echo "$CURRENT_HASH" > "$REQ_HASH_FILE"
+  else
+    echo "Dependencies are up to date, Skipping."
+  fi
 
   echo "Bot is starting..."
   python3 main.py
