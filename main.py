d223a83b6ffafc8e85e06805689217764e3c35b7
A_Duck <therealaduck@gmail.com>
2026-02-08 18:10:13 -0600

    setting up update and autorun

--- main.py
+++ main.py
@@ -1,25 +1,37 @@
 import os
 import sys
 import discord
+from discord import app_commands
 from discord.ext import commands
 
 TOKEN = os.getenv("YUURI_TOKEN")
 intents = discord.Intents.default()
 intents.members = True
 intents.message_content = True
-bot = commands.Bot(command_prefix="/", intents=intents)
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+TEST_GUILD_ID = 939897099024203778
 
 if TOKEN is None:
     raise RuntimeError("RUNTIME ERROR CODE1: Discord Token not found in the environment variable!")
 
+bot.event()
+async def on_ready():
+    print(f"Logged in as {bot.user}")
 
-bot.command(description='For if Yuuri misbehaves.')
-async def force_restart(ctx):
-    if bot.is_owner(ctx):
-        await ctx.send("force restart command received. restarting...")
+    guild = discord.Object(id=TEST_GUILD_ID)
+    bot.tree.copy_global_to(guild=guild)
+    await bot.tree.sync(guild=guild)
+
+    print("Slash commands synced")
+
+bot.tree.command(name="Force Restart", description='For if Yuuri misbehaves.')
+async def force_restart(interaction: discord.Interaction):
+    if not await bot.is_owner(interaction.user):
+        await interaction.response.send_message("Hey! Watch it! You cant tell me what to do!", ephemeral=True)
+        return
+        await interaction.response.send_message("Oh alright Ill do as you say.", ephemeral=True)
         await bot.close()
         sys.exit(0)
-    else:
-        await ctx.send("Hey! Watch it! You cant tell me what to do!")
 
 bot.run(TOKEN)
